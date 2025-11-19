5b831f2294cd57b632057b0a7cd82871b5240b79
KWON-WOO <ckw075@naver.com>
2025-11-19 18:38:43 +0900

    ✨ feat: 로그아웃 기능 추가.

--- src/main/java/com/springdiaryproject/springdiarydevelop/controller/LoginController.java
+++ src/main/java/com/springdiaryproject/springdiarydevelop/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.springdiaryproject.springdiarydevelop.dto.login.LoginDto;
 import com.springdiaryproject.springdiarydevelop.dto.login.LoginSessionInfo;
 import com.springdiaryproject.springdiarydevelop.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,15 @@ public class LoginController {
         LoginSessionInfo info = service.login(loginRequest);
         servletRequest.getSession().setAttribute("user", info);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@SessionAttribute(name = "user", required = false) LoginSessionInfo sessionUser, HttpSession session) {
+        if (sessionUser == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        session.invalidate();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
